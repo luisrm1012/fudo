9144e324deaba02ea795daa7414735d7522f2a3c
LUIS ALBERTO RASQUIN MARRA <100383346@alumnos.uc3m.es>
2022-02-14 17:34:20 +0100

    uploaded code

---
+++ script/script.js
@@ -0,0 +1,25 @@
+var title1 = document.querySelector(".title-p1");
+var title2 =  document.querySelector(".title-p2");
+
+function alternateTitle() {
+
+    if (title1.innerText.includes("不動")){
+        title1.innerText = "Fudo";
+        title2.innerText = "Investments";
+    }
+    else if (title1.innerText.includes("Fudo")){
+        title1.innerText = "不動投資";
+        title2.innerText = "";
+    }
+}
+
+document.querySelector(".header-container-items").addEventListener("click", e => {
+
+    document.querySelectorAll(".header-items a").forEach(element => {
+        element.classList.remove("active");
+    });
+
+    console.log(e.target)
+    e.target.classList.add("active");
+
+})
