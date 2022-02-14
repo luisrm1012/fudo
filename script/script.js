e54558488feee068e218a62094feac521fe6048c
LUIS ALBERTO RASQUIN MARRA <100383346@alumnos.uc3m.es>
2022-02-14 17:58:25 +0100

    fixed script loading too early problem

--- script/script.js
+++ script/script.js
@@ -1,25 +1,23 @@
 var title1 = document.querySelector(".title-p1");
-var title2 =  document.querySelector(".title-p2");
+var title2 = document.querySelector(".title-p2");
+
+console.log(document.querySelector(".header"));
 
 function alternateTitle() {
-
-    if (title1.innerText.includes("不動")){
+    if (title1.innerText.includes("不動")) {
         title1.innerText = "Fudo";
         title2.innerText = "Investments";
-    }
-    else if (title1.innerText.includes("Fudo")){
+    } else if (title1.innerText.includes("Fudo")) {
         title1.innerText = "不動投資";
         title2.innerText = "";
     }
 }
 
-document.querySelector(".header-container-items").addEventListener("click", e => {
-
-    document.querySelectorAll(".header-items a").forEach(element => {
+document.querySelector(".header-container-items").addEventListener("click", (e) => {
+    document.querySelectorAll(".header-items a").forEach((element) => {
         element.classList.remove("active");
     });
 
-    console.log(e.target)
+    console.log(e.target);
     e.target.classList.add("active");
-
-})
+});
